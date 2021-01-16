cb40f8dc9ad6c10e7326f5e0b8bc4a0fbc53b4ea
Codester Club <codesterclub@gmail.com>
2021-01-16 14:11:40 +0530

    update bfuck.c

--- bfuck.c
+++ bfuck.c
@@ -1,19 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-int indexOf( const char *cmd, char el, int from ) {
+#include <stdio.h>            // For printf() and scanf()
+#include <stdlib.h>           // For malloc() and calloc() 
+#include <string.h>           // For strcmp() and strlen()
+
+/* This method returns next index of an integer
+ * 
+ * char *cmd creates a reference to the [0]th posn of the string 
+ * passed in, which means, cmd stores the value of its memory address, 
+ * while *cmd stores the reference. Hence strings are passed as a 
+ * reference to methods.
+ * 
+ * Why char? 
+ * Because C has no string data type. *cmd indicates only one memory 
+ * location, also denoted by cmd[0]. Note that in this case we don't 
+ * use the *. *cmd is same as cmd[0].
+ * 
+ * On doing cmd[i], we access the address at RAM[cmd + i]. As it's 
+ * said previously, printing cmd gives the address as a hex. printing 
+ * cmd[0] gives the value at 0th. cmd[i] gives value i places beyond 
+ * cmd.
+ */
+int indexOf( char *cmd, char el, int from ) {
+	// Usual for loop, strlen() is 'string length'
 	for( int i = from; i < strlen( cmd ); i++) {
+		// This cmd[i] is previously explained
 		if( cmd[i] == el) {
 			return i;
 		}
 	}
 	return -1;
 }
+// Same as first method, we take in a string
 int input( char *cmd ) {
 	int i = 0;
+	// We assign ith posn of cmd to one single character input
 	do {
+		// getchar() gets next character from input stream
 		cmd[i] = getchar();
 	} while( cmd[i++] != '!' );
+	/* If you didn't know, now you do. All strings must end 
+	 * with a null character in c. The beginning is marked by address 
+	 * stored in cmd. But the end must be marked by a null character. 
+	 * Otherwise two strings in memory may get mixed up.
+	 */
 	cmd[i] = '\0';
 	return i - 1;
 }
