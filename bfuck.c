db6e1a775e089fec025da469a077ef158bd440c7
Codester Club <codesterclub@gmail.com>
2021-01-16 14:28:41 +0530

    update bfuck.c

--- bfuck.c
+++ bfuck.c
@@ -47,12 +47,28 @@ int input( char *cmd ) {
 	return i - 1;
 }
 int main() {
+	/*
+	 * calloc( 0, sizeof( char ) ): 0 is no. of memory blocks the string cmd will 
+	 * initially have. sizeof is NOT a function but an operator that returns in bytes 
+	 * the memory occupied by its operand. The operand may be a variable, or a data type.
+	 * The 2nd argument of calloc() is the size of each memory block.
+	 * 
+	 * calloc() automatically increases no. of memory blocks as required during runtime.
+	 * C has no new keyword. Dynamic allocation if memory is done by malloc() or calloc().
+	 */
+	// Command array that takes in the bfuck code in whole
 	char *cmd = calloc( 0, sizeof( char ) );
+	// Index of loop start posn in bfuck code
 	int LpIndex = 0;
+	// Data array to store bfuck I/O
 	int data[32];
+	// Data pointer that is used to select an index from data array and operate on it.
 	unsigned int ptr = 0;
+	// Instruction pointer traverses the cmd array
 	unsigned int i = 0;
+	// Take input using our custom function
 	input( cmd );
+	// Read bfuck rules if you don't know them.
 	do {
 		if( cmd[i] == '<' ) {
 			ptr--;
@@ -66,6 +82,7 @@ int main() {
 		else if( cmd[i] == '-' ) {
 			data[ptr]--;
 		}
+		// Work incomplete from here
 		else if( cmd[i] == '[' & data[ptr] == 0 ) {
 			LpIndex = i;
 			i = indexOf( cmd, ']', i + 1 ) + 1;
@@ -80,5 +97,21 @@ int main() {
 			scanf( "%d", &data[ptr] );
 		}
 	} while( cmd[i++] != '!' );
+	/* This frees up the memory allocated by calloc().
+	 * In our case since the program ends here, free() is not necessary 
+	 * as the system deletes the memory occupied by our program once 
+	 * execution ends.
+	 * But in case we had multiple arrays/strings allocated dynamically 
+	 * and we had huge data inout in them and we had a huge code where 
+	 * not all of them would be necessary, we will actually be wasting memory.
+	 * 
+	 * Also say we assign one array A to another array B. That means now B points 
+	 * to memory of A. But what happens to older memory of A?
+	 * In java, the garbage collector clears this automatically. In c, we must 
+	 * first free A using free(A) the we must do A = B;
+	 * 
+	 * Hence it is a good practice to free up memory when you realise you no longer 
+	 * need them in C.
+	 */
 	free(cmd);
 }
